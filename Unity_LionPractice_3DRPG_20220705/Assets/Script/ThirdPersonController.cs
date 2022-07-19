fae0294450ee5ef592bee51a7350a6f8b882ce3f
tomboy5566 <105775195+tomboy5566@users.noreply.github.com>
2022-07-19 16:32:45 +0800

    Update Jump

--- Unity_LionPractice_3DRPG_20220705/Assets/Script/ThirdPersonController.cs
+++ Unity_LionPractice_3DRPG_20220705/Assets/Script/ThirdPersonController.cs
@@ -19,6 +19,7 @@ namespace KuanLun
 
         private Animator ani;
         private CharacterController controller;
+        private string parRun = "走跑參數";
         #endregion
         #region 事件
         private void Awake()
@@ -30,6 +31,7 @@ namespace KuanLun
         private void Update()
         {
             Move();
+            Jump();
         }
         #endregion
         #region 功能
@@ -38,14 +40,34 @@ namespace KuanLun
             float v = Input.GetAxisRaw("Vertical");
             float h = Input.GetAxisRaw("Horizontal");
             transform.rotation = traCamera.rotation;
-            direction.z = -v;
-            direction.x = -h;
+            transform.eulerAngles = new Vector3(0, traCamera.eulerAngles.y, 0);
+            direction.z = v;
+            direction.x = h;
+            direction = transform.TransformDirection(direction);
             //print($"垂直方向: {v}");
             controller.Move(direction * move * Time.deltaTime);
+            float vAxis = Input.GetAxis("Vertical");
+            float hAxis = Input.GetAxis("Horizontal");
+            if (Mathf.Abs(vAxis) > 0.1f)
+            {
+                ani.SetFloat(parRun, Mathf.Abs(vAxis));
+            }
+            else if (Mathf.Abs(hAxis) > 0.1f)
+            {
+                ani.SetFloat(parRun, Mathf.Abs(hAxis));
+            }
+            else
+            {
+                ani.SetFloat(parRun, 0);
+            }
         }
         private void Jump()
         {
-
+            if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                direction.y = jump;
+            }
+            direction.y += Physics.gravity.y / 1.5f * Time.deltaTime;
         }
         #endregion
     }
